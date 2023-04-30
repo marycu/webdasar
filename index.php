0c3d889029ab51954972c80ad7af2db8e5fe7556
marycu <dindasyifa682@gmail.com>
2023-04-30 21:03:26 +0800

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama_kamu ="Dinda";
+$umur = 5;
+// tanda petik dua
+ /*
+ echo "Hello World, Welcome ".$nama_kamu;
+ echo " umur Kamu ".$umur;
+ echo " tahun ";
+ echo "<br/>";
+  */
+  echo "Hello World, Welcome ".$nama_kamu." Umur kamu ".$umur." tahun";
+  echo "<br/>";
+  echo "Berarti $nama_kamu lahir di tahun ";
+  echo 2023-$umur;
+  // tanda petik satu
+  // echo 'Hello World';
+?>
+ 
