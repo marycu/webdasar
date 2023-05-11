f59a00e0fccf517536c8fa13f93472611d224e09
marycu <dindasyifa682@gmail.com>
2023-05-11 19:01:30 +0800

    tambah data php

--- php-lanjutan.php
+++ php-lanjutan.php
@@ -16,9 +16,64 @@
     <h2><?=$tipe_data?></h2>   
 
     <?php
-        $pecahan = 5.85;
+        $pecahan = 5.85; // tipe data float/pecahan
     ?>  
 
     <h2>saya menampilkan nilai pecahan ini <?=$pecahan?></h2> 
+
+    <?php
+           $perbandingan = true; // tipe data boolean
+
+           echo $perbandingan; 
+            echo "<br/>";
+    ?>
+
+<?php
+
+/*
+   $provinsi[0] = "Jawa Timur";
+   $provinsi[1] = "DKI Jakarta";
+   $provinsi[2] = "IKN";
+*/
+
+    /* $provinsi = array("Jawa Timur", 
+                      "DKI Jakarta",
+                      "IKN"); */
+    
+        $provinsi;
+
+        $hasil = 5;
+
+            echo $hasil--;
+            echo "<br/>";
+            echo $hasil;
+            echo "<br/>";
+
+            define("SATU", 1);
+
+            echo SATU;
+
+            echo "<br/>";
+
+             /* if($perbandingan == true) {
+                echo "ucapkan selamat pagi";
+            } else {
+                echo "ucapkan selamat";
+            }
+            */
+            $perbandingan = 4;
+            switch($perbandingan) { // percabangan
+                case 1:
+                    $pesan = "soto ayam";
+                break;
+                case 2:
+                    $pesan = "ayam goreng";
+                break;
+                default:
+                    $pesan = "engga ding";
+            }
+ 
+
+    ?>
 </body>
 </html>
