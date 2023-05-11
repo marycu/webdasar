a957ebeb82d303748e385ad600ee9ad49665ab99
marycu <dindasyifa682@gmail.com>
2023-05-11 23:46:36 +0800

    tugas CRUD

---
+++ public/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$base = "mahasiswa";
+
+
+$conn = mysqli_connect($host, $user, $pass, $base);
+
+if (mysqli_connect_errno()) {
+    die("Failed To Connect");
+}
+?>
